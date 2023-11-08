fb6b784909724546d308a9e14e3912057ac813dd
youbutfromthepast <pedro.rodriguez17@utrgv.edu>
2023-11-07 18:22:33 -0600

    fixed errors, added keyword search

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def add_data_to_collection(data, collection):
         # grabs attributes from data.json
         title = game['title']
         genre = game['genre']
-        # console = game['console']
+        console = game['console']
         developer = game['developer']
         # esrb = game['esrb']
         rating = game['rating']
@@ -48,7 +48,7 @@ def add_data_to_collection(data, collection):
         description = game['description']
 
         # specific attributes being embedded
-        embeddable_string = f"{title}{genre}{developer}{rating}{description}"
+        embeddable_string = f"{title}{genre}{console}{developer}{rating}{description}"
         documents.append(embeddable_string)
 
         # stores as metadata
@@ -65,7 +65,7 @@ def add_data_to_collection(data, collection):
     )
 
 
-def get_results(query, dev_query, n_results=2, rating_minimum=0, rating_maximum=10):
+def get_results(query, dev_filter, console_filter=[], n_results=2, rating_minimum=0, rating_maximum=10):
     metadatas = []
 
     # adds condition of rating minimum and maximum
@@ -78,29 +78,31 @@ def get_results(query, dev_query, n_results=2, rating_minimum=0, rating_maximum=
         }
     ]
 
-    if dev_query:
-        dev_keywords = dev_query.split()
-        where_conditions.append({"developer": {"$in": dev_keywords}})
+    if dev_filter:
+        where_conditions.append({"developer": {"$in": dev_filter}})
 
-    # results = collection.query(query_texts=[query], n_results=n_results)
+    if console_filter:
+        print(console_filter)
+        where_conditions.append({"console": {"$in": console_filter}})
+
     results = collection.query(
         query_texts=[query],
         n_results=n_results,
         where={"$and": where_conditions}
     )
-    # results = collection.query(query_texts=["fajita"], n_results=3, where={"rating": "7"})
 
-    for i in range(n_results):
+    num_results = len(results["metadatas"][0])
+    for i in range(min(n_results, num_results)):
         metadatas.append(results["metadatas"][0][i])
 
     return metadatas
 
 
-def search(query, dev_query, n_results, rating_minimum, rating_maximum):
-    results = get_results(query, dev_query, n_results=n_results, rating_minimum=rating_minimum, rating_maximum=rating_maximum)
+def search(query, dev_filter, console_filter, n_results, rating_minimum, rating_maximum):
+    results = get_results(query, dev_filter, console_filter, n_results=n_results, rating_minimum=rating_minimum, rating_maximum=rating_maximum)
 
     try:
-        df = pd.DataFrame(results, columns=['title', 'genre', 'developer', 'rating', 'description'])
+        df = pd.DataFrame(results, columns=['title', 'genre', 'console', 'developer', 'rating', 'description'])
         return df
     except Exception as e:
         raise gr.Error(e.message)
@@ -116,7 +118,7 @@ collection = get_chroma_collection("games")
 add_data_to_collection(data, collection)
 
 # result check
-results = get_results("adventure", "SquareSoft Capcom", n_results=3, rating_minimum=0, rating_maximum=10)
+results = get_results("adventure", ["Capcom", "ChunSoft", "Nintendo"], ["Super Nintendo"], n_results=3, rating_minimum=0, rating_maximum=10)
 
 
 
@@ -129,12 +131,21 @@ with gr.Blocks() as demo:
         with gr.Row():
             with gr.Column():
                 query = gr.Textbox(label="What are you looking for?", lines=5)
-                dev_query = gr.Textbox(label = "Preferred developer?", lines=1)
+                dev_filter = gr.Dropdown(
+                    ["Nintendo", "SquareSoft", "ChunSoft", "Capcom", "Konami"],
+                    multiselect=True,
+                    label="Preferred developers?"
+                )
+                console_filter = gr.CheckboxGroup(
+                    ["NES", "Super Nintendo", "Playstation", "Dreamcast", "Playstation 3", "Xbox 360", "Playstation 4", "Nintendo Switch", "PC", "Game Boy", "Game Boy Color", "Game Boy Advance", "DS", "PSP", "3DS", "iOS", "Android"],
+                    label="Console filter",
+                    info="Specific consoles only?"
+                )
                 with gr.Row():
                     rating_minimum = gr.Slider(label="Minimum rating", minimum=0, maximum=10, value=0, step=1)
                     rating_maximum = gr.Slider(label="Maximum rating", minimum=0, maximum=10, value=10, step=1)
                 n_results = gr.Slider(label="Results to Display", minimum=0, maximum=10, value=2, step=1)
                 btn = gr.Button(value="Submit")
-                table = gr.Dataframe(label="Results", headers=['title', 'genre', 'developer', 'rating', 'description'], wrap=True)
-            btn.click(search, inputs=[query, dev_query, n_results, rating_minimum, rating_maximum], outputs=[table])
+                table = gr.Dataframe(label="Results", headers=['title', 'genre', 'console', 'developer', 'rating', 'description'], wrap=True)
+            btn.click(search, inputs=[query, dev_filter, console_filter, n_results, rating_minimum, rating_maximum], outputs=[table])
     demo.launch(share=True)
